7644e020671700d666edc0e4002f10964504496d
Chris <tof.giga@hotmail.fr>
2018-10-19 11:19:21 +0200

    ToastDemo

---
+++ app/src/main/java/m2iformation/fr/toastdemo/MainActivity.java
@@ -0,0 +1,30 @@
+package m2iformation.fr.toastdemo;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static m2iformation.fr.toastdemo.Tools.affToast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btLancer;
+    private TextView tvToast;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btLancer = findViewById(R.id.button);
+        tvToast = findViewById(R.id.tvToast);
+    }
+
+    public void lancer(View view) {
+        affToast(this, "Ceci est un exemple. Elle est belle la biscotte");
+    }
+}
